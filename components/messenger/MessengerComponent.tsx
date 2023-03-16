ed84cbf3c6b94535dbf61f3bffed6c4569c1f73c
Imamul Hassan <imamul.hassan@bjitgroup.com>
2023-03-16 12:19:36 +0600

    Laravel echo connected

--- components/messenger/MessengerComponent.tsx
+++ components/messenger/MessengerComponent.tsx
@@ -1,134 +1,188 @@
-'use client'
+"use client";
 
-import { getCookie } from 'cookies-next';
+import { getCookie } from "cookies-next";
 import React, { useContext, useEffect, useRef, useState } from "react";
 import "./Messenger.css";
 import axios from "axios";
-import Conversation from '../conversations/Conversations';
-import Message from '../message/Message';
-import ChatOnline from '../chatOnline/ChatOnline';
-import useCurrentUser from '../../hooks/useCurrentUser.hook';
-import useChat from '../../hooks/useChat.hook';
-  
+import Conversation from "../conversations/Conversations";
+import Message from "../message/Message";
+import ChatOnline from "../chatOnline/ChatOnline";
+import useCurrentUser from "../../hooks/useCurrentUser.hook";
+import useChat from "../../hooks/useChat.hook";
 
 const Messenger = () => {
-    // consts
-    const {id, token} = useCurrentUser()
-    const {updateChannelNames} = useChat();
-    
-    // states
-    const scrollRef=useRef<any>()
-    const [conversations, setConversations]=useState<any[]>([])
-    const [currentChat, setCurrentChat]=useState<any>(null)
-    const [messages, setMessages]=useState<any>([])
-    const [newMessage, setNewMessage]=useState<any>("")
-    const [onlineUsers, setOnlineUsers]=useState<any>([])
+  // consts
+  const { id, token } = useCurrentUser();
+  const { updateChannelNames, conversations, setConversations } = useChat();
 
-   
+  // states
+  const scrollRef = useRef<any>();
+  const [currentChat, setCurrentChat] = useState<any>(null);
+  const [newMessage, setNewMessage] = useState<any>("");
+  const [onlineUsers, setOnlineUsers] = useState<any>([]);
+  const [messages, setMessages] = useState<any[]>([]);
 
-    // functions
-    async function getConversations(token: string) {
-        try {
-            const result=await axios.get(`http://localhost:8000/api/v1/my-conversations`, {
-                headers: {
-                    Authorization: `Bearer ${token}`
-                }
-            })
-            if(!result) {
-                return
-            }
-            updateChannelNames(result.data.result.map((convo: any) => `chat.${convo.id}`))
-            setConversations(result.data.result);
-        } catch (error) {
-            console.log(error);
+  // functions
+  async function getConversations(token: string) {
+    try {
+      const result = await axios.get(
+        `http://localhost:8000/api/v1/my-conversations`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
         }
+      );
+      if (!result.data.result) {
+        return;
+      }
+      updateChannelNames(
+        result.data?.result?.map((convo: any) => `chat.${convo.id}`)
+      );
+      setConversations(result.data?.result || []);
+      setCurrentChat(result.data.result[0]);
+      getAllAndSetAvailableUsers();
+    } catch (error) {
+      console.log(error);
     }
-    
-    async function postMessage(data:any) {
-        try {
-            const result=await axios.post(`http://localhost:8000/api/v1/message/store`,data, {
-                headers: {
-                    Authorization: `Bearer ${token}`
-                }
-            })
-            setMessages([...messages, result.data.result]);
-            setNewMessage("")
-        } catch (error) {
-            console.log(error);
+  }
+
+  async function postMessage(data: any) {
+    try {
+      const result = await axios.post(
+        `http://localhost:8000/api/v1/message/store`,
+        data,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
         }
+      );
+      //   setMessages([...messages, result.data.result]);
+      setNewMessage("");
+    } catch (error) {
+      console.log(error);
     }
+  }
 
-    function handleSubmit(e:any){
-        e.preventDefault()
-        const message={
-            conversation_id: currentChat.id,
-            sender_id: id,
-            text: newMessage,
+  async function getAllAndSetAvailableUsers() {
+    try {
+      const result = await axios.get(
+        `http://localhost:8000/api/v1/users/list`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
         }
-        postMessage(message)
+      );
+      if (!result || !conversations.length) {
+        return;
+      }
+      const chattedPersonIdList = conversations.flatMap((convo) =>
+        convo.members.map((member: any) => member.id)
+      );
+      const availableUsers = result.data.result.data.filter(
+        (user: any) => !chattedPersonIdList.includes(user.id)
+      );
+      setOnlineUsers(availableUsers);
+    } catch (error) {
+      console.log(error);
     }
+  }
 
-    // useEffects
-    useEffect(()=>{
-        if(token){
-            getConversations(token)
-        }
-    },[token])
+  function handleSubmit(e: any) {
+    e.preventDefault();
+    const message = {
+      conversation_id: currentChat.id,
+      sender_id: id,
+      text: newMessage,
+    };
+    postMessage(message);
+  }
 
-    useEffect(()=>{
-        if(!currentChat) {
-            return
-        }
-        setMessages(conversations.filter((convo) => convo.id === currentChat.id).flatMap((convo) => convo.messages))
-    },[currentChat])
+  // useEffects
+  useEffect(() => {
+    if (token) {
+      getConversations(token);
+    }
+  }, [token]);
 
-    useEffect(()=>{
-     scrollRef.current?.scrollIntoView({behavior:"smooth"})
-    },[messages])
-
-    console.log(messages)
-    return (
-        <>
-            <div className="messenger">
-                <div className="chatMenu">
-                    <div className="chatMenuWrapper">
-                        <input type="text" placeholder="Search for friends"  className="chatMenuInput"/>
-                        {
-                            conversations?.map((c:any)=><div onClick={()=>setCurrentChat(c)}><Conversation conversation={c}/></div>)
-                        }
-                    </div>
-                </div>
-                <div className="chatBox">
-                    <div className="chatBoxWrapper">
-                        {
-                            currentChat ?
-                            <>
-                            <div className="chatBoxTop">
-                                {
-                                    messages.map((m:any)=>
-                                    <div ref={scrollRef}><Message message={m} own={m.sender === id}/></div>
-                                    )
-                                }
-                            </div>
-                            <div className="chatBoxBottom">
-                            <textarea className="chatMessageInput" placeholder="Write something..." onChange={(e)=>setNewMessage(e.target.value)} value={newMessage} ></textarea>
-                            <button className="chatSubmitButton" onClick={handleSubmit}>Send</button>
-                        </div>
-                            </>
-                            :<span className="noConversationText">Open a conversation to start a chat</span>
-                        }
-                        
-                        
-                    </div>
-                </div>
-                <div className="chatOnline">
-                    <div className="chatOnlineWrapper">
-                        <ChatOnline onlineUsers={onlineUsers} currentUserId={id} setCurrentChat={setCurrentChat}/>
-                    </div>
-                </div>
-            </div>
-        </>
+  useEffect(() => {
+    if (!currentChat) {
+      return;
+    }
+    setMessages(
+      conversations
+        .filter((convo) => convo.id === currentChat.id)
+        .flatMap((convo) => convo.messages)
     );
+    // getAllAndSetAvailableUsers();
+  }, [currentChat, conversations]);
+
+  useEffect(() => {
+    scrollRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, [messages]);
+
+  return (
+    <>
+      <div className="messenger">
+        <div className="chatMenu">
+          <div className="chatMenuWrapper">
+            <input
+              type="text"
+              placeholder="Search for friends"
+              className="chatMenuInput"
+            />
+            {conversations?.map((c: any) => (
+              <div onClick={() => setCurrentChat(c)}>
+                <Conversation conversation={c} />
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="chatBox">
+          <div className="chatBoxWrapper">
+            {currentChat ? (
+              <>
+                <div className="chatBoxTop">
+                  {messages.map((m: any) => (
+                    <div ref={scrollRef}>
+                      <Message message={m} own={m.sender_id === id} />
+                    </div>
+                  ))}
+                </div>
+                <div className="chatBoxBottom">
+                  <textarea
+                    className="chatMessageInput"
+                    placeholder="Write something..."
+                    onChange={(e) => setNewMessage(e.target.value)}
+                    value={newMessage}
+                  ></textarea>
+                  <button className="chatSubmitButton" onClick={handleSubmit}>
+                    Send
+                  </button>
+                </div>
+              </>
+            ) : (
+              <span className="noConversationText">
+                Open a conversation to start a chat
+              </span>
+            )}
+          </div>
+        </div>
+        <div className="chatOnline">
+          <div className="chatOnlineWrapper">
+            <ChatOnline
+              onlineUsers={onlineUsers}
+              setConversations={setConversations}
+              currentUserId={id}
+              setCurrentChat={setCurrentChat}
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Messenger;
--- hooks/useChat.hook.ts
+++ hooks/useChat.hook.ts
@@ -1,12 +1,11 @@
 import Pusher from 'pusher-js';
 import Echo from 'laravel-echo';
-import { useMemo, useState, useEffect } from 'react';
+import { useMemo, useState, useEffect, useLayoutEffect } from 'react';
 import useCurrentUser from './useCurrentUser.hook';
-//@ts-ignore
-window.Pusher = Pusher;
 
-function options(token: string) { 
-    return{
+
+function options(token: string) {
+    return {
         broadcaster: 'pusher',
         key: 'scvbmzkmvcscvbmzkmvcscvbmzkmvcbjit',
         cluster: 'mt1',
@@ -24,34 +23,51 @@ function options(token: string) {
 };
 
 const useChat = (...initialChannels: string[]) => {
-    const {token} = useCurrentUser()
+    const { id, token } = useCurrentUser()
     const echo = useMemo(() => new Echo(options(token)), [token]);
     const [channels, setChannels] = useState<string[]>(initialChannels)
-    const [messages, setMessages] = useState<any[]>([]);
-    const [input, setInput] = useState('');
-    console.log(echo.socketId())
-    
-    const updateChannelNames = (channels: string[]) => {
+    const [currentChat, setCurrentChat] = useState<any>(null)
+    const [conversations, setConversations] = useState<any[]>([])
+
+    function updateChannelNames(channels: string[]) {
         setChannels(channels)
     }
 
+    function connectChatChannel(channelName: string) {
+        echo.private(channelName).listen(
+            '.chatEvent',
+            (data: any) => {
+                console.log(47, data);
+                setConversations((prev) => prev.map((convo) => data.conversation_id !== convo.id ? convo : { ...convo, messages: [...convo.messages, data] }))
+            }
+        );
+    }
+
+    function connectNewConversationChannel(userId: number) {
+        echo.private(`new.conversation.user.${userId}`).listen('.newConvsersationEvent', (data: any) => { console.log(data); setConversations((prev) => [data, ...prev]) });
+    }
+
     useEffect(() => {
-        if(channels.length < 1) {
+        if (channels.length < 1) {
             return
         }
 
-        for(let i = 0; i < channels.length; i++) {
-            echo.private(channels[i]).listen(
-                'SendMessage',
-                (data: any) => {
-                  console.log( data);
-                  setMessages((prev) => [data, ...prev])
-                }
-            );
+        for (let i = 0; i < channels.length; i++) {
+            connectChatChannel(channels[i])
         }
     }, [channels]);
 
-    return { messages, input, setInput, updateChannelNames };
-  };
+    useEffect(() => {
+        connectNewConversationChannel(id);
+    }, [id])
 
-  export default useChat;
+    useEffect(() => {
+        //@ts-ignore
+        window.Pusher = Pusher;
+    }, [])
+
+    console.log(conversations)
+    return { updateChannelNames, connectChatChannel, conversations, setConversations, currentChat, setCurrentChat };
+};
+
+export default useChat;
